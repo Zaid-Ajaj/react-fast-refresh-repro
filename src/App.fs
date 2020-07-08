fe1edd27a31074391cafd58aa77af139167783b6
Zaid <zaid.naom@gmail.com>
2020-07-08 11:04:04 +0200

    Initial commit

---
+++ src/App.fs
@@ -0,0 +1,24 @@
+module App
+
+open Feliz
+open Browser.Dom
+
+let counter = React.functionComponent(fun () ->
+    let (count, setCount) = React.useState(0)
+    Html.div [
+        Html.button [
+            prop.style [ style.marginRight 5 ]
+            prop.onClick (fun _ -> setCount(count + 1))
+            prop.text "Increment!"
+        ]
+
+        Html.button [
+            prop.style [ style.marginLeft 5 ]
+            prop.onClick (fun _ -> setCount(count - 1))
+            prop.text "Decrement"
+        ]
+
+        Html.h1 count
+    ])
+
+ReactDOM.render(counter, document.getElementById "feliz-app")
